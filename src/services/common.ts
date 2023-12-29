f68059c12d1aefb3512b4227a599b5675688cb65
ha1dysh <olekshaid@gmail.com>
2023-12-29 13:48:51 +0200

    fake random

--- src/services/common.ts
+++ src/services/common.ts
@@ -1,6 +1,20 @@
+import { Stock } from "./store";
+
 export function intl(v: number) {
 	return new Intl.NumberFormat("en-US", {
 		style: "currency",
 		currency: "USD",
 	}).format(v);
+}
+
+export function fakeRandom(stock: Stock) {
+	const roundToDecimal = (value: number) => Math.round(value * 10) / 10;
+	const applyFluctuation = (original: number) =>
+		roundToDecimal(original + (Math.random() - 0.5) * 1);
+
+	return {
+		high: applyFluctuation(stock.high),
+		low: applyFluctuation(stock.low),
+		prevClose: applyFluctuation(stock.prevClose),
+	};
 }
